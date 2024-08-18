647ccf963eb582f1d6100de06675725c0161b0cf
Javadoff <sabuhi.javadli@solid.az>
2024-08-19 01:09:48 +0400

    test: burger class

---
+++ tests/test_burger.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.pytemplate.domain.models import Burger
+
+
+def test_burger_with_all_ingredients():
+    burger = Burger("Wheat", "Beef", ["Mayo", "Ketchup"], ["Lettuce", "Tomato"])
+    assert str(burger) == "Wheat Beef ['Mayo', 'Ketchup'] ['Lettuce', 'Tomato'] "
+
+
+def test_burger_without_toppings():
+    burger = Burger("Wheat", "Beef", ["Mayo", "Ketchup"])
+    assert str(burger) == "Wheat Beef ['Mayo', 'Ketchup'] No toppings "
+
+
+def test_burger_without_sauce():
+    burger = Burger("Wheat", "Beef", toppings=["Lettuce", "Tomato"])
+    assert str(burger) == "Wheat Beef No sauce ['Lettuce', 'Tomato'] "
+
+
+def test_burger_without_sauce_and_toppings():
+    burger = Burger("Wheat", "Beef")
+    assert str(burger) == "Wheat Beef No sauce No toppings "
